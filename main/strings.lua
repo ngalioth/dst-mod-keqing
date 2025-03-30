3a0cef309f1bf91bbb7f1f435a40716ceac794b6
Alioth <ngalioth@outlook.com>
2025-03-30 16:54:59 +0800

    feat: 🎸 STRINGS changes

--- main/strings.lua
+++ main/strings.lua
@@ -1,13 +1,17 @@
 -- The character select screen lines  --人物选人界面的描述
-STRINGS.CHARACTER_TITLES.keqing = "七星之玉衡" -- 人物描述
+STRINGS.CHARACTER_TITLES.keqing =
+    "璃月七星之一，玉衡星。对「帝君一言而决的璃月」颇有微词——但实际上，神挺欣赏她这样的人。" -- 人物描述
 STRINGS.CHARACTER_NAMES.keqing = "刻晴" -- 人物名字
-STRINGS.CHARACTER_DESCRIPTIONS.keqing = "*沾染风霜的发簪\n*抵天雷罚\n*工作狂" -- 人物标签
+STRINGS.CHARACTER_DESCRIPTIONS.keqing =
+    "位移、能量回复、派遣、自身附魔、自身伤害提升、自身暴击率提升"
+
+-- *沾染风霜的发簪\n*抵天雷罚\n*工作狂" -- 人物标签
 STRINGS.CHARACTER_SURVIVABILITY.keqing = "较为轻松" -- 生存几率
 STRINGS.CHARACTER_QUOTES.keqing = "\"耽误太多时间，事情可就做不完了。\"" -- 选人界面下面的一句话
 
 -- The character's name as appears in-game  --人物在游戏里面的名字
 STRINGS.NAMES.keqing = "刻晴" -- 人物名字
---STRINGS.SKIN_NAMES.keqing_none = "霓裾翩跹"  --检查界面显示的名字
+-- STRINGS.SKIN_NAMES.keqing_none = "霓裾翩跹"  --检查界面显示的名字
 
 STRINGS.SKIN_NAMES.keqing = "雷厉寰宇"
 STRINGS.SKIN_QUOTES.keqing = "\"耽误太多时间，事情可就做不完了。\""
@@ -32,88 +36,94 @@ STRINGS.SKIN_DESCRIPTIONS.keqing_telepole = "哈！吾将以雷电击碎一切�
 -- Custom speech strings  ----人物语言文件  可以进去自定义
 STRINGS.CHARACTERS.KEQING = require("speech_keqing")
 
-STRINGS.NAMES.KQ_GOLDENSHRIMP = "金丝虾球"  -- 物体在游戏中显示的名字
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_GOLDENSHRIMP = "金黄酥脆的土豆丝包裹的弹嫩虾仁，我都不敢想象那有多好吃。" -- 按alt检查时人物说的话
+STRINGS.NAMES.KQ_GOLDENSHRIMP = "金丝虾球" -- 物体在游戏中显示的名字
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_GOLDENSHRIMP =
+    "金黄酥脆的土豆丝包裹的弹嫩虾仁，我都不敢想象那有多好吃。" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_POCKETCAKE = "口袋饼"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_POCKETCAKE = "口袋饼" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_POCKETCAKE = "肉馅多样的面饼，方便易携。" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_GRILLEDFISH = "烤吃虎鱼"  -- 物体在游戏中显示的名字
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_GRILLEDFISH = "急火烤制的鱼串，金灿灿的表皮下是鲜嫩的美味。" -- 按alt检查时人物说的话
+STRINGS.NAMES.KQ_GRILLEDFISH = "烤吃虎鱼" -- 物体在游戏中显示的名字
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_GRILLEDFISH =
+    "急火烤制的鱼串，金灿灿的表皮下是鲜嫩的美味。" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_FRIEDEGG = "提瓦特煎蛋"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_FRIEDEGG = "提瓦特煎蛋" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_FRIEDEGG = "单面煎熟的禽蛋，有着阳光般和煦的口感。" -- 按alt检查时人物说的话
 
 STRINGS.NAMES.KQ_SPECIALFISH = "绝境求生烤鱼" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_SPECIALFISH = "｡＾･؂･＾｡" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_LIYUEPOT = "寻仙的美食家"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_LIYUEPOT = "寻仙的美食家" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_LIYUEPOT = "仙人煮饭，三教九流之辈，勿扰。" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_LIYUEPOT_ITEM = "寻仙的美食家"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_LIYUEPOT_ITEM = "寻仙的美食家" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_LIYUEPOT_ITEM = "仙人煮饭，三教九流之辈，勿扰。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.KQ_LIYUEPOT_ITEM = "锅的制作描述" -- 物品的制作描述
 
-STRINGS.NAMES.GREENSWORD = "磐岩结绿"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.GREENSWORD = "磐岩结绿" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.GREENSWORD = "血洗的碧绿剑锋依然闪亮如新。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.GREENSWORD = "翠玉琢成之剑" -- 物品的制作描述
 STRINGS.ACTIONS.CASTSPELL.GREENSWORD = "滑铲！" -- ?
 
-STRINGS.NAMES.KQ_SHRIMP = "虾"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_SHRIMP = "虾" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_SHRIMP = "画风像是沃拓克斯从隔壁搬过来的。" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_COOKEDSHRIMP = "熟虾"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_COOKEDSHRIMP = "熟虾" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_COOKEDSHRIMP = "为什么煮熟之后眼睛上没有出现“X”？" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_DEADSHRIMP = "虾仁"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_DEADSHRIMP = "虾仁" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_DEADSHRIMP = "我能吃上一整天！" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_COOKEDDEADSHRIMP = "熟虾仁"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_COOKEDDEADSHRIMP = "熟虾仁" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_COOKEDDEADSHRIMP = "虾仁的营养是虾仁的七倍。" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_SHRIMPHEAD = "虾头"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_SHRIMPHEAD = "虾头" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_SHRIMPHEAD = "神奇的“X”出现了！" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_STEAMEDSHRIMPHEAD = "蒸虾头"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_STEAMEDSHRIMPHEAD = "蒸虾头" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_STEAMEDSHRIMPHEAD = "虾头蝻蒸虾头，回复666" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_AMBER = "石珀"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_AMBER = "石珀" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_AMBER = "极纯的岩元素结晶，亦称为“岩之心”。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.KQ_AMBER = "一种岩元素结晶" -- 物品的制作描述
 
-STRINGS.NAMES.KQ_STONEAMBER = "石珀矿石"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_STONEAMBER = "石珀矿石" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_STONEAMBER = "高纯的岩元素伴生的矿石，闪着琥珀的光泽。" -- 按alt检查时人物说的话
 
-STRINGS.NAMES.KQ_LIGHTAMBER = "仙家琥珀—刚烈自见"  -- 物体在游戏中显示的名字
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_LIGHTAMBER = "传说中源自仙家的琥珀，历经风雨打磨呈现出剑锋与枪尖的形貌。" -- 按alt检查时人物说的话
+STRINGS.NAMES.KQ_LIGHTAMBER = "仙家琥珀—刚烈自见" -- 物体在游戏中显示的名字
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_LIGHTAMBER =
+    "传说中源自仙家的琥珀，历经风雨打磨呈现出剑锋与枪尖的形貌。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.KQ_LIGHTAMBER = "曾有镇邪之力，散落在璃月各地的琥珀" -- 物品的制作描述
 
-STRINGS.NAMES.KQ_FOODBAG = "零柒式—便携营养袋"  -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_FOODBAG = "零柒式—便携营养袋" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_FOODBAG = "能将大部分美食转化为能瞬间摄入的[营养物质]。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.KQ_FOODBAG = "一个便携返鲜冰箱"
 
-STRINGS.NAMES.KQ_BACKPACK = "刻刻猫猫喵喵包包"  -- 物体在游戏中显示的名字
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_BACKPACK = "喵喵喵，喵喵，喵喵！" -- 按alt检查时人物说的话
+STRINGS.NAMES.KQ_BACKPACK = "刻刻猫猫喵喵包包" -- 物体在游戏中显示的名字
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_BACKPACK =
+    "灵巧而富有智慧的生灵，抱持着与生俱来的优雅与骄傲。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.KQ_BACKPACK = "灵巧而富有智慧的生灵，抱持着与生俱来的优雅与骄傲。" -- 物品的制作描述
 
-STRINGS.NAMES.KQ_HAIRPINS = "刻晴的发簪" -- 物体在游戏中显示的名字
+STRINGS.NAMES.KQ_HAIRPINS = "绝境求生发簪" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_HAIRPINS = "它沾染风霜的模样，才是最有魅力的。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.KQ_HAIRPINS = "与刻晴形影不离的配饰" -- 物品的制作描述
 
 STRINGS.NAMES.KQ_LIGHTSTONE = "流明石触媒" -- 物体在游戏中显示的名字
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_LIGHTSTONE = "但愿它能如同掌心的小太阳一样，为黑暗中的跋涉带来些许光亮、些许慰藉。" -- 按alt检查时人物说的话
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_LIGHTSTONE =
+    "但愿它能如同掌心的小太阳一样，为黑暗中的跋涉带来些许光亮、些许慰藉。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.KQ_LIGHTSTONE = "为黑暗中的跋涉带来些许光亮。" -- 物品的制作描述
 
 STRINGS.NAMES.KQ_ELECFUR = "奇特的羽毛" -- 物体在游戏中显示的名字
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_ELECFUR = "带着这枚羽毛，走遍世界吧。毕竟，已经和他说好了。" -- 按alt检查时人物说的话
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_ELECFUR =
+    "带着这枚羽毛，走遍世界吧。毕竟，已经和他说好了。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.KQ_ELECFUR = "在雷电的土地上能够发挥神奇的作用。" -- 物品的制作描述
 
 STRINGS.NAMES.KQ_HOTBOTTLE = "放热瓶" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_HOTBOTTLE = "为渴求温暖的人提供慰藉。" -- 按alt检查时人物说的话
 STRINGS.RECIPE_DESC.KQ_HOTBOTTLE = "释放热量的瓶子。" -- 物品的制作描述
 
---STRINGS.NAMES.KQ_COLDBOTTLE = "制冷瓶" -- 物体在游戏中显示的名字
---STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_COLDBOTTLE = "但愿它能如同掌心的小太阳一样，为黑暗中的跋涉带来些许光亮、些许慰藉。" -- 按alt检查时人物说的话
+-- STRINGS.NAMES.KQ_COLDBOTTLE = "制冷瓶" -- 物体在游戏中显示的名字
+-- STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_COLDBOTTLE = "但愿它能如同掌心的小太阳一样，为黑暗中的跋涉带来些许光亮、些许慰藉。" -- 按alt检查时人物说的话
 
 STRINGS.NAMES.KQ_HOTCORE = "常燃火种" -- 物体在游戏中显示的名字
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.KQ_HOTCORE = "持续燃烧的种子。" -- 按alt检查时人物说的话
